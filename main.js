43149e05a541fe2de2cf8468c18913e6db344e95
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2022-04-07 11:46:21 +0100

    updated files

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let num = [1,2,3,4,5,6,7,8,9,10,11,12];
+//listing the zones in an array
 
-function multiply(b){
-    for(i=0; i<num.length; i++){
-        let result = num[i] * b;
-        console.log(`${num[i]} * ${b} = ${result}`);
-    }   
-}
-multiply(2);
+let North_Central = [" Niger", "Benue", "Nassarawa","Plateau","Kogi", "Kwara"];
+let  North_West = ["Jagawa", "Kano", "Katsina", "Kaduna", "Kebbi", "Zamfara", "Sokoto"];
+let North_East = [ "Gombe", "Bauchi", "Yobe", "Borno", "Adamawa", "Taraba" ];
+let South_South = ["Akwa-Ibom", "Cross-River", "Bayelsa", "Rivers", "Delta", "Edo"];
+let South_East = ["Abia", "Imo", "Ebonyi", "Enugu", "Anambara"];
+let South_West = ["Ekiti", "Ondo", "Osun", "Oyo", "Ogun", "Lagos"]
 
-
+//Printing the number of states in each zone
+console.log(North_Central.length);
+console.log(North_West.length);
+console.log(North_East.length);
+console.log(South_South.length);
+console.log(South_East.length);
+console.log(South_West.length);
