711c96c730f398b8125ecc7b7b9a83cfeda1a2bd
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2022-04-11 22:27:08 +0100

    updated files

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
-//listing the zones in an array
 
-let North_Central = [" Niger", "Benue", "Nassarawa","Plateau","Kogi", "Kwara"];
-let  North_West = ["Jagawa", "Kano", "Katsina", "Kaduna", "Kebbi", "Zamfara", "Sokoto"];
-let North_East = [ "Gombe", "Bauchi", "Yobe", "Borno", "Adamawa", "Taraba" ];
-let South_South = ["Akwa-Ibom", "Cross-River", "Bayelsa", "Rivers", "Delta", "Edo"];
-let South_East = ["Abia", "Imo", "Ebonyi", "Enugu", "Anambara"];
-let South_West = ["Ekiti", "Ondo", "Osun", "Oyo", "Ogun", "Lagos"]
+//listing the zones in a multi-dimensional array
 
-//Printing the number of states in each zone
-console.log(North_Central.length);
-console.log(North_West.length);
-console.log(North_East.length);
-console.log(South_South.length);
-console.log(South_East.length);
-console.log(South_West.length);
+const zone = [
+    [" Niger", "Benue", "Nassarawa","Plateau","Kogi", "Kwara"],
+    ["Jagawa", "Kano", "Katsina", "Kaduna", "Kebbi", "Zamfara", "Sokoto"],
+    [ "Gombe", "Bauchi", "Yobe", "Borno", "Adamawa", "Taraba" ],
+    ["Akwa-Ibom", "Cross-River", "Bayelsa", "Rivers", "Delta", "Edo"],
+    ["Abia", "Imo", "Ebonyi", "Enugu", "Anambara"],
+    ["Ekiti", "Ondo", "Osun", "Oyo", "Ogun", "Lagos"]
+];
+
+console.log(zone[0].length);
+console.log(zone[1].length);
+console.log(zone[2].length);
+console.log(zone[3].length);
+console.log(zone[4].length);
+console.log(zone[5].length);
+
+
+//multiplication table
+let num = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+function multiply(b){
+
+    for(i=0; i<num.length; i++) {
+        b = prompt('input a number to get the multiplication table:' );
+        let result = num[i] * b;
+        console.log(`${num[i]} * ${b} = ${result}`); 
+    }
+}
+multiply();
+
+
+
+
+
+
+//COMPUTING AREA OF A CIRCLE
+
+/*let PIE = 3.14;
+let r;
+function area(r) {
+    console.log ( 'Area =' + " " + PIE * Math.pow(r, 2));
+} */
+// area(4);
+
+//OBJECT
+
+/* const dev ={
+    id: '1',
+
+    skills:{
+        php:'good'
+    }
+}; */
+
+// console.log(dev.skills["php"]);
