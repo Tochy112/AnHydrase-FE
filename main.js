fcdfa7a18e9b82f9353992efc687e213594dc66b
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2022-04-14 07:47:43 +0100

    updated files

--- main.js
+++ main.js
@@ -1,58 +1,40 @@
 
-//listing the zones in a multi-dimensional array
+// DEFINITION OF MAP IN JS
+/*  Map is a collection of elements where each element is stored as a Key, value pair.
+    Map object can hold both objects and primitive values as either key or value. 
+    When we iterate over the map object it returns the key, value pair in the same order as inserted.
+    A Map has a property that represents the size of the map. */
 
-const zone = [
-    [" Niger", "Benue", "Nassarawa","Plateau","Kogi", "Kwara"],
-    ["Jagawa", "Kano", "Katsina", "Kaduna", "Kebbi", "Zamfara", "Sokoto"],
-    [ "Gombe", "Bauchi", "Yobe", "Borno", "Adamawa", "Taraba" ],
-    ["Akwa-Ibom", "Cross-River", "Bayelsa", "Rivers", "Delta", "Edo"],
-    ["Abia", "Imo", "Ebonyi", "Enugu", "Anambara"],
-    ["Ekiti", "Ondo", "Osun", "Oyo", "Ogun", "Lagos"]
-];
+// CREATING A MAP
+    /* We can create a JavaScript Map by:
+        Passing an Array to new Map()
+        Create a Map and use Map.set() 
 
-console.log(zone[0].length);
-console.log(zone[1].length);
-console.log(zone[2].length);
-console.log(zone[3].length);
-console.log(zone[4].length);
-console.log(zone[5].length);
+        Example1:
+        const fruits = new Map([
+            ["apples", 500],
+            ["bananas", 300],
+            ["oranges", 200]
+        ]);
 
+        Example2:
+        //create a map
+        const fruits = new Map();
 
-//multiplication table
-let num = [1,2,3,4,5,6,7,8,9,10,11,12];
-
-function multiply(b){
-
-    for(i=0; i<num.length; i++) {
-        b = prompt('input a number to get the multiplication table:' );
-        let result = num[i] * b;
-        console.log(`${num[i]} * ${b} = ${result}`); 
-    }
-}
-multiply();
+        // Set Map Values
+        fruits.set("apples", 500);
+        fruits.set("bananas", 300);
+        fruits.set("oranges", 200);
+    */
 
 
+    //FIVE EXAMPLE OF MAP METHODS
 
+   /*  
+        keys()	Returns an iterator object with the keys in a Map
+        values()	Returns an iterator object of the values in a Map
+        clear()	Removes all the elements from a Map
+        delete()	Removes a Map element specified by a key
+        get()	Gets the value for a key in a Map
 
-
-
-//COMPUTING AREA OF A CIRCLE
-
-/*let PIE = 3.14;
-let r;
-function area(r) {
-    console.log ( 'Area =' + " " + PIE * Math.pow(r, 2));
-} */
-// area(4);
-
-//OBJECT
-
-/* const dev ={
-    id: '1',
-
-    skills:{
-        php:'good'
-    }
-}; */
-
-// console.log(dev.skills["php"]);
+*/
